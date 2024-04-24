206a55e90d2e674fe86ad8221fde8f6ef9a691fc
adam <01187561@pw.edu.pl>
2024-04-24 22:43:29 +0200

    Aktualizacja struktury pliku binarnego

--- src/bin.h
+++ src/bin.h
@@ -1,5 +1,6 @@
 #ifndef _BIN_H
 #define _BIN_H
+#include <stdint.h>
 typedef struct bin{
 	
 	int fileid;
@@ -10,11 +11,15 @@ typedef struct bin{
 	short int entry_y;
 	short int exit_x;
 	short int exit_y;
+	uint64_t res1;
+	uint32_t res2;
 	int counter;
 	int sollution_off;
 	char separator;
 	char wall;
 	char path;
+	int solveid;
+	uint16_t steps;
 } bin_t;
 
 
